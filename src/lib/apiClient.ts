e3935b62aa45f3ff6e1d64fa80bea87757051e67
jaridgeway <jamesaridgeway@gmail.com>
2026-01-11 21:50:40 -0500

    Add type safety to api client

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,11 @@
 import { useSessionStore } from '@/stores/session'
 const API_BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:8000'
 
-export async function apiClient<T = any>(endpoint: string, options: RequestInit = {}, retry:boolean = true):Promise<T | null>  {
+export async function apiClient<T = any>(
+  endpoint: string,
+  options: RequestInit = {},
+  retry: boolean = true,
+): Promise<T> {
   const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
     ...options,
     headers: {
@@ -9,17 +13,16 @@ export async function apiClient<T = any>(endpoint: string, options: RequestInit 
       ...options.headers,
     },
   })
-if (response.status === 401 && retry) {
-    return retryApiClient(endpoint,options)
+  if (response.status === 401 && retry) {
+    return retryApiClient<T>(endpoint, options)
   }
-  
 
   if (!response.ok) {
     let errorBody = null
     try {
       errorBody = await response.json()
     } catch {
-      errorBody="Something went wrong."
+      errorBody = 'Something went wrong.'
     }
 
     const error: any = new Error('API Error')
@@ -30,13 +33,13 @@ if (response.status === 401 && retry) {
 
   // Handle 204 No Content
   if (response.status === 204) {
-    return null
+    return undefined as T
   }
 
   return response.json()
 }
 
-async function retryApiClient (endpoint: string, options: RequestInit = {}) {
+async function retryApiClient<T>(endpoint: string, options: RequestInit = {}):Promise<T> {
   const sessionStore = useSessionStore()
   try {
     await sessionStore.refreshAccessToken()
