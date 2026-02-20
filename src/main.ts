23aecb70973061b777cf7004572ab514cf44a7af
daniel-grinevich <kgbdannn@gmail.com>
2026-02-19 19:00:25 -0600

    tanstack query no retries in dev and a little bit less on auth

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,32 @@ import router from './router'
 
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
+const vueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        retry: (failureCount: number, error: any) => {
+          if (import.meta.env.VITE_ENVIRONMENT === 'dev') {
+            return false
+          }
+
+          if (error.response?.status === 401) {
+            return failureCount < 2
+          }
+
+          return failureCount < 3
+        },
+      },
+    },
+  },
+}
+
 import '@stripe/stripe-js'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(VueQueryPlugin)
+app.use(VueQueryPlugin, vueQueryPluginOptions)
 
 app.mount('#app')
